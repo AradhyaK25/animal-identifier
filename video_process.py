d99a72e5cba4c715ed6352ef5925d38faec027d6
Pranav Siddharth <pranavsid98@gmail.com>
2017-10-07 02:33:00 +0530

    video module

---
+++ video_process.py
@@ -0,0 +1,17 @@
+import cv2
+import os
+
+def record_frames(video_path):
+    os.makedirs("video_results/%s" % video_path)
+    vidcap = cv2.VideoCapture(video_path)
+    success, image = vidcap.read()
+    count = 0
+    success = True
+    while success:
+        success, image = vidcap.read()
+        print ('Read a new frame: ', success)
+        if count %2 == 0:
+            cv2.imwrite("video_results/%s/frame%d.jpg" % (video_path, count), image)
+        count += 1
+
+# record_frames('sample_vid.mp4')
